c8cc4dfc573726ef24e488b4a5d1a73887737169
Stephen2212 <36001416+Stephen2212@users.noreply.github.com>
2018-01-31 15:17:57 +0000

    Add files via upload

---
+++ ai insulter.py
@@ -0,0 +1,38 @@
+"""
+This is an AI insulter python program.
+Type an insult if you dare.
+
+"""
+insult=input("Type insult now") 
+place=input("type a place")
+verb=input("type a verb")
+
+
+
+print("You were born a " + (insult) + " in " + (place) + " and had to " + (verb) + " to survive.")
+print(" (       ) (         (             (      ____ ")
+print(" )\ ) ( /( )\ )      )\ )  *   )   )\ )  |   / ")
+print("(()/( )\()|()/(   ( (()/(` )  /(( (()/(  |  /  ")
+print(" /(_)|(_)\ /(_))  )\ /(_))( )(_))\ /(_)) | /   ")
+print("(_))  _((_|_)) _ ((_|_)) (_(_()|(_|_))_  |/    ")
+print("|_ _|| \| / __| | | | |  |_   _| __|   \(      ")
+print(" | | | .` \__ \ |_| | |__  | | | _|| |) )\     ")
+print("|___||_|\_|___/\___/|____| |_| |___|___((_)    ") 
+                                                
+
+
+""" your text will look something like this:
+
+You were born a computer in microsoft and had to program to survive
+
+ (       ) (         (             (      ____ 
+ )\ ) ( /( )\ )      )\ )  *   )   )\ )  |   / 
+(()/( )\()|()/(   ( (()/(` )  /(( (()/(  |  /  
+ /(_)|(_)\ /(_))  )\ /(_))( )(_))\ /(_)) | /   
+(_))  _((_|_)) _ ((_|_)) (_(_()|(_|_))_  |/    
+|_ _|| \| / __| | | | |  |_   _| __|   \(      
+ | | | .` \__ \ |_| | |__  | | | _|| |) )\     
+|___||_|\_|___/\___/|____| |_| |___|___((_)    
+                                               
+"""
+
