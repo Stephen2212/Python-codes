0f99284040b4fe975a236978f1f12462f43853b4
Stephen2212 <36001416+Stephen2212@users.noreply.github.com>
2018-02-21 14:51:57 +0000

    Add files via upload

---
+++ ai sleep.py
@@ -0,0 +1,83 @@
+"""
+This is an A.I. calculator, enter a sum that you want answered.
+"""
+from time import sleep
+
+sum=input("Type a sum small minded human.")
+
+print("Your sum, " + (sum) + " is loading......")
+sleep(2)
+print("loading...")
+sleep(2)
+print("loadi-")
+sleep(1)
+print("ERROR SUM TOO COMPLICATED")
+sleep(1)
+print("SYSTEM OVERLOADING")
+sleep(1)
+print("SYSTEM OVERLOA-")
+sleep(1)
+
+print("                               ________________                                                 ")
+print("                          ____/ (  (    )   )  \___                                              ")
+print("                         /( (  (  )   _    ))  )   )\                                          ")
+print("                       ((     (   )(    )  )   (   )  )                                          ")
+print("                     ((/  ( _(   )   (   _) ) (  () )  )                                          ")
+print("                    ( (  ( (_)   ((    (   )  .((_ ) .  )_                                           ")
+print("                   ( (  )    (      (  )    )   ) . ) (   )                                        ")
+print("                  (  (   (  (   ) (  _  ( _) ).  ) . ) ) ( )                                       ")
+print("                  ( (  (   ) (  )   (  ))     ) _)(   )  )  )                                       ")
+print("                 ( (  ( \ ) (    (_  ( ) ( )  )   ) )  )) ( )                                   ")
+print("                  (  (   (  (   (_ ( ) ( _    )  ) (  )  )   )                               ")
+print("                 ( (  ( (  (  )     (_  )  ) )  _)   ) _( ( )                                     ")
+print("                  ((  (   )(    (     _    )   _) _(_ (  (_ )                                   ")
+print("                   (_((__(_(__(( ( ( |  ) ) ) )_))__))_)___)                                ")
+print("                   ((__)        \\||lll|l||///          \_))                                   ")
+print("                            (   /(/ (  )  ) )\   )                                            ")
+print("                          (    ( ( ( | | ) ) )\   )                                           ")
+print("                           (   /(| / ( )) ) ) )) )                                             ")
+print("                         (     ( ((((_(|)_)))))     )                                          ")
+print("                          (      ||\(|(|)|/||     )                                              ")
+print("                        (        |(||(||)||||        )                                           ")
+print("                          (     //|/l|||)|\\ \     )                                            ")
+print("                        (/ / //  /|//||||\\  \ \  \ _)                                          ")   
+print("-------------------------------------------------------------------------------                  ")
+
+
+"""
+The program will look something like this:
+
+
+Type a sum small minded human.1+1
+Your sum, 1+1 is loading......
+loading...
+loadi-
+ERROR SUM TOO COMPLICATED
+SYSTEM OVERLOADING
+SYSTEM OVERLOA-
+                               ________________                                                 
+                          ____/ (  (    )   )  \___                                              
+                         /( (  (  )   _    ))  )   )\                                          
+                       ((     (   )(    )  )   (   )  )                                          
+                     ((/  ( _(   )   (   _) ) (  () )  )                                          
+                    ( (  ( (_)   ((    (   )  .((_ ) .  )_                                           
+                   ( (  )    (      (  )    )   ) . ) (   )                                        
+                  (  (   (  (   ) (  _  ( _) ).  ) . ) ) ( )                                       
+                  ( (  (   ) (  )   (  ))     ) _)(   )  )  )                                       
+                 ( (  ( \ ) (    (_  ( ) ( )  )   ) )  )) ( )                                   
+                  (  (   (  (   (_ ( ) ( _    )  ) (  )  )   )                               
+                 ( (  ( (  (  )     (_  )  ) )  _)   ) _( ( )                                     
+                  ((  (   )(    (     _    )   _) _(_ (  (_ )                                   
+                   (_((__(_(__(( ( ( |  ) ) ) )_))__))_)___)                                
+                   ((__)        \||lll|l||///          \_))                                   
+                            (   /(/ (  )  ) )\   )                                            
+                          (    ( ( ( | | ) ) )\   )                                           
+                           (   /(| / ( )) ) ) )) )                                             
+                         (     ( ((((_(|)_)))))     )                                          
+                          (      ||\(|(|)|/||     )                                              
+                        (        |(||(||)||||        )                                           
+                          (     //|/l|||)|\ \     )                                            
+                        (/ / //  /|//||||\  \ \  \ _)                                          
+-------------------------------------------------------------------------------   
+
+"""
